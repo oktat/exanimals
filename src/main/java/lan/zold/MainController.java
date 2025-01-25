1c9c6dc66d71ed6ed2656b752f6c708d8b06fe35
Sallai András <andrewteki@gmail.com>
2025-01-25 22:14:23 +0100

    Elkezdve

---
+++ src/main/java/lan/zold/MainController.java
@@ -0,0 +1,60 @@
+package lan.zold;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.GridPane;
+
+public class MainController {
+
+    String[] animals = {
+        "kutya",
+        "macska",
+        "oroszlán",
+        "elefánt",
+        "zebra",
+        "delfin",
+        "tyúk",
+        "panda",
+        "tigris",
+        "kígyó",
+        "ló",
+        "tehén",
+        "majom",
+        "bárány",
+        "pingvin",
+        "kaméleon",
+        "süni",
+        "róka",
+        "vaddisznó",
+        "bölény"
+    };
+
+    CheckBox[] animalCheckboxes = new CheckBox[animals.length];
+
+    @FXML
+    private GridPane grid;    
+
+    @FXML
+    void initialize() {
+        for (int i = 0; i < animals.length; i++) {
+            animalCheckboxes[i] = new CheckBox(animals[i]);
+        }
+        for (int i = 0; i < animalCheckboxes.length; i++) {
+            grid.add(animalCheckboxes[i], i % 5, i / 5);
+        }
+    }
+
+    @FXML
+    void onClickSaveButton(ActionEvent event) {
+        System.out.println("save");
+
+        for (CheckBox checkBox : animalCheckboxes) {
+            if (checkBox.isSelected()) {
+                System.out.println(checkBox.getText());
+            }
+        }
+
+    }
+
+}
